8d602c58770f25d4c154ef3e466db4815e1395b4
GLeb Tarakanov <glebtgd@gmail.com>
2023-02-16 10:51:15 +0600

    Первоначальная версия проекта, где созданы три шаблона html, добавлены первые стили и две картинки. Были прописаны urls для каждого из шаблонов и соотвествующие вьюшки

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
 from webapp.views.base import index_view
+from webapp.views.tasks import tasks_view, add_view, detail_view
 
 urlpatterns = [
-    path('', index_view)
+    path('', index_view),
+    path('tasks', tasks_view),
+    path('task', detail_view),
+    path('tasks/add', add_view)
 ]
---
+++ source/webapp/views/tasks.py
@@ -0,0 +1,28 @@
+from django.core.handlers.wsgi import WSGIRequest
+from django.shortcuts import render, redirect
+from webapp.models import Task
+
+def tasks_view(request):
+    tasks = Task.objects.all()
+    context = {
+        'tasks': tasks
+    }
+    return render(request, 'tasks.html', context=context)
+
+def detail_view(request):
+    task_pk = request.GET.get('pk')
+    task = Task.objects.get(pk=task_pk)
+    context = {'task': task}
+    return render(request, 'task_detail.html', context=context)
+
+def add_view(request: WSGIRequest):
+    if request.method == 'GET':
+        return render(request, 'add_task.html')
+    task_data = {
+        'title': request.POST.get('title'),
+        'description': request.POST.get('description'),
+        'status': request.POST.get('status'),
+        'ended_at': request.POST.get('date')
+    }
+    task = Task.objects.create(**task_data)
+    return redirect(f'/task?pk={task.pk}')
