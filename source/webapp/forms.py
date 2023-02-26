ad973b60333ebb0f318adc7ff6e3d3bc29a0b8d7
GLeb Tarakanov <glebtgd@gmail.com>
2023-02-26 14:40:42 +0600

    Допилены шерховатости

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -15,9 +15,13 @@ class TaskForm(forms.ModelForm):
             'status': 'Статус',
             'ended_at': 'Срок выполнения'
         }
+        help_texts = {
+            "ended_at":  "Введите дату в формате YYYY-MM-DD!"
+        }
 
     def clean_title(self):
         title = self.cleaned_data.get('title')
         if len(title) < 2:
             raise ValidationError('Заголовок должен быть длинее 2-ух символов')
         return title
+
